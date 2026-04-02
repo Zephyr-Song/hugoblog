19dbc0dd9727cc43eb4233c154f1479ff1c7a76c
Zephyr-Song <Zephyr-Song@users.noreply.github.com>
2026-04-02 00:54:20 +0000

    deploy: 07b8ad409ad10512c93ce575338fa38b792aedd7

--- js/music-loader.js
+++ js/music-loader.js
@@ -8,8 +8,7 @@
     el.setAttribute('type', 'playlist');
     el.setAttribute('id', '2809513713');
     el.setAttribute('fixed', 'true');
-    el.setAttribute('mini', 'false');
-    el.setAttribute('list-folded', 'false');
+    el.setAttribute('mini', 'true');
     el.setAttribute('autoplay', 'false');
     el.setAttribute('theme', '#8fb3a9');
     document.body.appendChild(el);
