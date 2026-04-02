33312955a2a9cab9a0b7f314664d1e2610ab7a60
Zephyr-Song <Zephyr-Song@users.noreply.github.com>
2026-04-02 07:26:52 +0000

    deploy: 4d288b98b5bb101b323e26cecd8a5288dc238780

--- js/music-loader.js
+++ js/music-loader.js
@@ -1,10 +1,10 @@
 /**
- * Music Loader - 单曲背景音乐
+ * Music Loader - 歌单播放（跳过前7首）
  */
 (function () {
   // 手机端不加载播放器
   var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || window.innerWidth < 1024;
-  
+
   if (isMobile) {
     return;
   }
@@ -12,13 +12,36 @@
   function initMeting() {
     var el = document.createElement('meting-js');
     el.setAttribute('server', 'netease');
-    el.setAttribute('type', 'song');
-    el.setAttribute('id', '190137');  // 网易云单曲ID，可以换成你想要的
+    el.setAttribute('type', 'playlist');
+    el.setAttribute('id', '2829816518');
     el.setAttribute('fixed', 'true');
     el.setAttribute('mini', 'true');
     el.setAttribute('autoplay', 'false');
+    el.setAttribute('order', 'random');
     el.setAttribute('theme', '#8fb3a9');
     document.body.appendChild(el);
+
+    // 歌单加载后移除前7首
+    function removeFirstSeven() {
+      var apEl = document.querySelector('.aplayer');
+      if (apEl && apEl.__aplayer && apEl.__aplayer.list && apEl.__aplayer.list.audios) {
+        var ap = apEl.__aplayer;
+        for (var i = 0; i < 7 && ap.list.audios.length > 1; i++) {
+          ap.list.remove(0);
+        }
+        ap.list.show();
+        return true;
+      }
+      return false;
+    }
+
+    var count = 0;
+    var timer = setInterval(function () {
+      count++;
+      if (removeFirstSeven() || count > 60) {
+        clearInterval(timer);
+      }
+    }, 500);
   }
 
   if (document.readyState === 'loading') {
